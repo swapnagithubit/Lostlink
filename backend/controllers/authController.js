f8753f4c1a3c54246f91ad8c8f4752dc5b886e29
Your Name <your-email@example.com>
2026-05-02 12:53:24 +0530

    fix gmail ipv4 smtp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,10 @@ const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
